892df77b5eec715fa0282b05a331a1d72bf927a9
Antonina1812 <146942940+Antonina1812@users.noreply.github.com>
2023-10-25 14:17:32 +0300

    Добавлена часть кода
    Добавлено 10 функций return

--- main.py
+++ main.py
@@ -24,6 +24,57 @@ def open_q1():
     dpg.configure_item("q1", show=True)
     dpg.set_primary_window("q1",True)
 
+def return_1():
+    dpg.configure_item("1c", show=False)
+    dpg.configure_item("main", show=True)
+    dpg.set_primary_window("main", True)
+
+def return_2():
+    dpg.configure_item("pascal", show=False)
+    dpg.configure_item("main", show=True)
+    dpg.set_primary_window("main", True)
+
+def return_3():
+    dpg.configure_item("cpp", show=False)
+    dpg.configure_item("main", show=True)
+    dpg.set_primary_window("main", True)
+
+def return_4():
+    dpg.configure_item("swift", show=False)
+    dpg.configure_item("main", show=True)
+    dpg.set_primary_window("main", True)
+
+def return_5():
+    dpg.configure_item("csharp", show=False)
+    dpg.configure_item("main", show=True)
+    dpg.set_primary_window("main", True)
+
+def return_6():
+    dpg.configure_item("python", show=False)
+    dpg.configure_item("main", show=True)
+    dpg.set_primary_window("main", True)
+
+def return_7():
+    dpg.configure_item("javascript", show=False)
+    dpg.configure_item("main", show=True)
+    dpg.set_primary_window("main", True)
+
+def return_8():
+    dpg.configure_item("php", show=False)
+    dpg.configure_item("main", show=True)
+    dpg.set_primary_window("main", True)
+
+def return_9():
+    dpg.configure_item("java", show=False)
+    dpg.configure_item("main", show=True)
+    dpg.set_primary_window("main", True)
+
+def return_0():
+    dpg.configure_item("who", show=False)
+    dpg.configure_item("main", show=True)
+    dpg.set_primary_window("main", True)
+
+
 
 # Вопрос 1
 with dpg.window(no_resize=True, no_title_bar=True, show=False,
